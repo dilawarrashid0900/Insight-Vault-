14a5400c1aa81185dc6cb7fb12e9eedae4d6d9cc
dilawarrashid0900 <dilawarrashid202131@gmail.com>
2025-02-23 13:50:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        button.addEventListener('click', function() {
+            alert('Item added to cart!');
+        });
+    });
+});
